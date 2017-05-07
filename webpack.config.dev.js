a49f209f96108949aaab9a1e669f60f104c4cd84
Pablo Puga <pablo.puga.git@gmail.com>
2017-05-08 01:24:57 +0200

    Enabled webpack hot reload

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+const spawn = require('child_process').spawn;
 const port = process.env.SERVER_PORT || 8080;
-const publicPath = "http://localhost:" + port + "/dist";
+const publicPath = "http://127.0.0.1:" + port + "/dist";
 
 module.exports = {
     entry: [
-        "webpack-dev-server/client?" + publicPath,
+        "webpack-dev-server/client?" + "http://127.0.0.1:" + port,
+        "webpack/hot/only-dev-server",
         "./src/js/app.js"
     ],
     output: {
@@ -17,9 +20,24 @@ module.exports = {
             { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin({})
+    ],
     devServer: {
+        hot: true,
         port: port,
         inline: true,
-        contentBase: path.resolve(__dirname, "app")
+        compress: true,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        contentBase: path.resolve(__dirname, "dist"),
+        setup() {
+            spawn(
+                'npm',
+                ['run', 'dev-app'],
+                { shell: true, env: process.env, stdio: 'inherit' }
+            )
+            .on('close', code => process.exit(code))
+            .on('error', spawnError => console.error(spawnError));
+        }
     }
 };
